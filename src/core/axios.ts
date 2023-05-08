08600b0b8bb42f54b723833ca0cdc182179d3374
Dalikk <dav@sciberia.io>
2023-05-08 22:53:55 +0900

    chore: axios - split to instances, add env check and microservice availability

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -3,8 +3,35 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
+const checkEnvVar = (varName: string): string => {
+  const value = process.env[varName];
+  if (!value) {
+    throw new Error(`${varName} env var required`);
+  }
+  return value;
+};
+
+const checkMicroserviceAvailability = async (url: string, name: string) => {
+  try {
+    await axios.get(url);
+    return true;
+  } catch (error) {
+    console.warn(`Microservice --${name}-- is not available now`);
+  }
+};
+
+const konturAPIHandlerBaseURL = checkEnvVar('KONTUR_API_HANDLER_BASE_URL');
+const cashAccountingBaseURL = checkEnvVar('CASH_ACCOUNTING_BASE_URL');
+
+checkMicroserviceAvailability(konturAPIHandlerBaseURL, 'Kontur API handler');
+checkMicroserviceAvailability(cashAccountingBaseURL, 'Cash accounting');
+
 const konturHandlerAPI = axios.create({
-  baseURL: process.env.KONTUR_API_HANDLER_BASE_URL,
+  baseURL: konturAPIHandlerBaseURL,
 });
 
-export { konturHandlerAPI };
+const cashAccountingAPI = axios.create({
+  baseURL: cashAccountingBaseURL,
+});
+
+export { konturHandlerAPI, cashAccountingAPI };
