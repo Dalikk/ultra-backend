70925e129acaf9f42ea9a710b17ce689f7aee382
Dalikk <dav@sciberia.io>
2023-05-08 21:07:07 +0900

    fix: parse .env to axiosClient

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const axiosClient = axios.create();
-console.warn(process.env.API_BASE_URL);
 axiosClient.defaults.baseURL = process.env.API_BASE_URL;
 
 // axiosClient.interceptors.request.use((config) => {
